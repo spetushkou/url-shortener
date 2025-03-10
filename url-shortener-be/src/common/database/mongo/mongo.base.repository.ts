067f1ae01b2386fa82b427cb6210d5b4ed74371f
spetushkov <sergey.petushkov@gmail.com>
2025-03-10 16:23:33 +0100

    feat: :rocket: #NOREF-0

---
+++ url-shortener-be/src/common/database/mongo/mongo.base.repository.ts
@@ -0,0 +1,29 @@
+import { FilterQuery, Model, UpdateQuery } from 'mongoose';
+import { MongoDocument } from './mongo.document';
+import { MongoEntity } from './mongo.entity';
+
+export abstract class MongoBaseRepository<Entity extends MongoDocument, Dto extends MongoEntity> {
+  constructor(protected readonly model: Model<Entity>) {}
+
+  async findMany(filter: FilterQuery<Entity> = {}): Promise<Entity[]> {
+    return this.model.find(filter).lean<Entity[]>(true);
+  }
+
+  async findOne(filter: FilterQuery<Entity>): Promise<Entity | null> {
+    return this.model.findOne(filter).lean<Entity>(true);
+  }
+
+  async create(createDto: Dto): Promise<Entity> {
+    const entity = new this.model(createDto);
+    const entitySaved = await entity.save();
+    return entitySaved.toJSON() as Entity;
+  }
+
+  async update(filter: FilterQuery<Entity>, updateDto: UpdateQuery<Entity>): Promise<Entity | null> {
+    return await this.model.findOneAndUpdate(filter, { $set: updateDto }, { new: true }).lean<Entity>(true);
+  }
+
+  async delete(filter: FilterQuery<Entity>): Promise<Entity | null> {
+    return this.model.findOneAndDelete(filter).lean<Entity>(true);
+  }
+}
