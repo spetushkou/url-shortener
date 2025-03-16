ec27b43e9032afd4ffd26c5d248669492317b584
spetushkov <sergey.petushkov@gmail.com>
2025-03-16 12:04:19 +0100

    feat: :rocket: #NOREF-0

--- url-shortener-fe/src/app/layout/layout.header.tsx
+++ url-shortener-fe/src/app/layout/layout.header.tsx
@@ -1,6 +1,8 @@
 import LinkIcon from '@mui/icons-material/Link';
 import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { AuthBar } from '../auth/bar/auth.bar';
+import { RouterPath } from '../router/router.path';
 
 export function LayoutHeader() {
   return (
@@ -12,15 +14,20 @@ export function LayoutHeader() {
         marginBottom: 6,
       }}
     >
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant='h4'>URL Shortener</Typography>
-        <LinkIcon sx={{ marginLeft: 2, fontSize: 40 }} />
+      <Box>
+        <Link
+          to={RouterPath.Home}
+          style={{
+            textDecoration: 'none',
+            color: 'inherit',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant='h4'>URL Shortener</Typography>
+          <LinkIcon sx={{ marginLeft: 2, fontSize: 40 }} />
+        </Link>
       </Box>
       <Box>
         <AuthBar />
