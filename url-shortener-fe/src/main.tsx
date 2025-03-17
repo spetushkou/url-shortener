6b743b4644f7f94d4fd1c65f75cc8392457550c0
spetushkov <sergey.petushkov@gmail.com>
2025-03-17 14:15:10 +0100

    feat: :rocket: #NOREF-0

--- url-shortener-fe/src/main.tsx
+++ url-shortener-fe/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/app';
 import './main.css';
 
+console.log('hi');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
