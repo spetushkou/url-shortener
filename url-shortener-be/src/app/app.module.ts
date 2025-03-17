666fc6bfcf285223b9baa836f6f2abbee5319a1e
spetushkov <sergey.petushkov@gmail.com>
2025-03-17 12:27:22 +0100

    feat: :rocket: #NOREF-0

--- url-shortener-be/src/app/app.module.ts
+++ url-shortener-be/src/app/app.module.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ExceptionGlobalFilterProvider } from '../common/exception/exception.global.filter.provider';
 import { PinoLoggerModule } from '../common/logger/pino/pino.logger.module';
+import { ThrottleGlobalProvider } from '../common/throttle/throttle.global.provider';
 import { DatabaseMongoModule } from '../database/mongo/database.mongo.module';
 import { UrlModule } from '../url/url.module';
 import { UserModule } from '../user/user.module';
@@ -21,32 +22,30 @@ import { AppConfigValidationSchema } from './app.config.validation.schema';
       cache: true,
       expandVariables: true,
     }),
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          name: 'short', // 3 calls in a second
-          ttl: 1000,
-          limit: 3,
-        },
-        {
-          name: 'medium', // 20 calls in 10 seconds
-          ttl: 10000,
-          limit: 20,
-        },
-        {
-          name: 'long', // 100 calls in a minute
-          ttl: 60000,
-          limit: 100,
-        },
-      ],
-    }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     PinoLoggerModule,
     DatabaseMongoModule,
     UserModule,
     AuthModule,
     UrlModule,
   ],
-  providers: [ExceptionGlobalFilterProvider],
+  providers: [ExceptionGlobalFilterProvider, ThrottleGlobalProvider],
 })
 export class AppModule implements OnModuleDestroy {
   async onModuleDestroy(): Promise<void> {
