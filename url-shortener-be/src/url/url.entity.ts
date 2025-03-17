682bb1641fcc44bbbd74078e22364e7d88dbb97f
spetushkov <sergey.petushkov@gmail.com>
2025-03-17 11:55:59 +0100

    feat: :rocket: #NOREF-0

--- url-shortener-be/src/url/url.entity.ts
+++ url-shortener-be/src/url/url.entity.ts
@@ -1,8 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { MongoDocument } from '../common/database/mongo/mongo.document';
+import { Url as UrlType } from './url';
 
 @Schema({ versionKey: false })
-export class Url extends MongoDocument {
+export class Url extends MongoDocument implements UrlType {
   @Prop({ required: true, unique: true })
   originalUrl: string;
 
--- url-shortener-be/src/user/user.entity.ts
+++ url-shortener-be/src/user/user.entity.ts
@@ -1,8 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { MongoDocument } from '../common/database/mongo/mongo.document';
+import { User as UserType } from './user';
 
 @Schema({ versionKey: false })
-export class User extends MongoDocument {
+export class User extends MongoDocument implements UserType {
   @Prop({ required: true, unique: true })
   email: string;
 
